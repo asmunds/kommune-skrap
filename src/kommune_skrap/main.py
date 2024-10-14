75d44d9ded8a7e513a0708ed7df0a85fecc724ba
Åsmund Frantzen Skomedal <asmund.frantzen.skomedal@aenergi.no>
2024-10-14 19:01:47 +0200

    Autosort imports by Ruff

--- src/kommune_skrap/main.py
+++ src/kommune_skrap/main.py
@@ -1,14 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import (
-    UnexpectedAlertPresentException,
     NoAlertPresentException,
+    UnexpectedAlertPresentException,
 )
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 __author__ = "Åsmund Frantzen Skomedal"
 __copyright__ = "Åsmund Frantzen Skomedal"
