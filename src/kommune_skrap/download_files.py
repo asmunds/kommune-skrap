69272f4fd04846177142aca42ef9fa844757ac30
Åsmund Frantzen Skomedal <asmund.frantzen.skomedal@aenergi.no>
2024-10-17 19:38:08 +0200

    Make all searches first, then download

--- src/kommune_skrap/download_files.py
+++ src/kommune_skrap/download_files.py
@@ -88,12 +88,15 @@ def main(url: str) -> None:
     # Wait until the dropdown is present
     wait = WebDriverWait(driver, 10)
 
+    # Make empty list for all link data
+    all_link_data_list = []
+
     # Prompt the user for input to continue or exit
     while (
-        input('\nMake relevant search, then press enter. Write "exit" when done. ')
+        input('\nMake relevant search, then press enter. Write "done" when done... ')
         .strip()
         .lower()
-        != "exit"
+        != "done"
     ):
         # Get the results
         resultater = driver.find_element(By.ID, "resultater")
@@ -107,95 +110,96 @@ def main(url: str) -> None:
             }
             for link in links
         ]
+        all_link_data_list.extend(link_data_list)
 
-        # Loop through all links to find the relevant data
-        for link_data in link_data_list:
-            link_url = link_data.get("url")
-            date_text = link_data.get("date").replace("/", ".")
+    # Loop through all links to find the relevant data
+    for link_data in all_link_data_list:
+        link_url = link_data.get("url")
+        date_text = link_data.get("date").replace("/", ".")
 
-            # Click on the link to enter a new web page
-            driver.execute_script("window.open();")
-            driver.switch_to.window(driver.window_handles[-1])
-            driver.get(link_url)
+        # Click on the link to enter a new web page
+        driver.execute_script("window.open();")
+        driver.switch_to.window(driver.window_handles[-1])
+        driver.get(link_url)
 
-            # Wait until the new page is loaded
-            wait.until(lambda driver: driver.current_url != URL)
-            wait.until(lambda driver: driver.find_element(By.TAG_NAME, "body"))
-            # Wait until the number of links is greater than 2
-            wait.until(lambda driver: len(driver.find_elements(By.TAG_NAME, "a")) > 2)
+        # Wait until the new page is loaded
+        wait.until(lambda driver: driver.current_url != URL)
+        wait.until(lambda driver: driver.find_element(By.TAG_NAME, "body"))
+        # Wait until the number of links is greater than 2
+        wait.until(lambda driver: len(driver.find_elements(By.TAG_NAME, "a")) > 2)
 
-            # Find and click all '+' signs to reveal hidden data
-            plus_signs = driver.find_elements(
-                By.CSS_SELECTOR, "span.glyphicon.glyphicon-plus[aria-hidden='true']"
+        # Find and click all '+' signs to reveal hidden data
+        plus_signs = driver.find_elements(
+            By.CSS_SELECTOR, "span.glyphicon.glyphicon-plus[aria-hidden='true']"
+        )
+        for plus_sign in plus_signs:
+            plus_sign.click()
+
+        # Find table called dagsordenDetaljer
+        dagsorden_detaljer = driver.find_element(By.ID, "dagsordenDetaljer")
+
+        # Get all the rows in tbody of dagsordenDetaljer
+        rows = dagsorden_detaljer.find_elements(By.XPATH, ".//tbody/tr")
+
+        # Loop through all the rows in the table to find the relevant data
+        for row in rows:
+            # Get the header of the current row
+            section_title = row.find_element(
+                By.XPATH, ".//h2[contains(@class, 'overskrift')]"
+            ).text
+
+            # Use the identify_keyword function to check for keywords in the section title
+            keyword_found = identify_keyword(
+                section_title=section_title, keywords=KEYWORDS
             )
-            for plus_sign in plus_signs:
-                plus_sign.click()
+            if keyword_found:
+                # Get the new links that have appeared after clicking the plus sign
+                # Find links that are located in this row of the table
+                subpage_links = row.find_elements(By.XPATH, ".//a")
+                subpage_links_dict = [
+                    {
+                        "href": link.get_attribute("href"),
+                        "text": link.text,
+                    }
+                    for link in subpage_links
+                ]
+                # Loop through the new links to find the one that contains the PDF
+                for subpage_link in subpage_links_dict:
+                    new_href = subpage_link.get("href")
+                    if (
+                        new_href
+                        and new_href.endswith("Pdf=false")
+                        and "Link til sak" in subpage_link.get("text")
+                    ):
+                        # Modify the URL to replace "Pdf=false" with "Pdf=true"
+                        pdf_url = new_href.replace("Pdf=false", "Pdf=true")
 
-            # Find table called dagsordenDetaljer
-            dagsorden_detaljer = driver.find_element(By.ID, "dagsordenDetaljer")
+                        # Open the pdf link in a new tab
+                        driver.execute_script("window.open();")
+                        driver.switch_to.window(driver.window_handles[-1])
+                        driver.get(pdf_url)
 
-            # Get all the rows in tbody of dagsordenDetaljer
-            rows = dagsorden_detaljer.find_elements(By.XPATH, ".//tbody/tr")
+                        # Wait until the PDF is loaded
+                        wait.until(lambda driver: driver.current_url != link_url)
 
-            # Loop through all the rows in the table to find the relevant data
-            for row in rows:
-                # Get the header of the current row
-                section_title = row.find_element(
-                    By.XPATH, ".//h2[contains(@class, 'overskrift')]"
-                ).text
+                        # Make download directory if it doesn't exist
+                        download_dir = DOWNLOAD_DIR / date_text
+                        if not download_dir.exists():
+                            download_dir.mkdir(parents=False)
 
-                # Use the identify_keyword function to check for keywords in the section title
-                keyword_found = identify_keyword(
-                    section_title=section_title, keywords=KEYWORDS
-                )
-                if keyword_found:
-                    # Get the new links that have appeared after clicking the plus sign
-                    # Find links that are located in this row of the table
-                    subpage_links = row.find_elements(By.XPATH, ".//a")
-                    subpage_links_dict = [
-                        {
-                            "href": link.get_attribute("href"),
-                            "text": link.text,
-                        }
-                        for link in subpage_links
-                    ]
-                    # Loop through the new links to find the one that contains the PDF
-                    for subpage_link in subpage_links_dict:
-                        new_href = subpage_link.get("href")
-                        if (
-                            new_href
-                            and new_href.endswith("Pdf=false")
-                            and "Link til sak" in subpage_link.get("text")
-                        ):
-                            # Modify the URL to replace "Pdf=false" with "Pdf=true"
-                            pdf_url = new_href.replace("Pdf=false", "Pdf=true")
+                        # Download the PDF file
+                        download_pdf(
+                            url=pdf_url,
+                            download_dir=download_dir,
+                            new_filename=section_title.replace("/", "_"),
+                        )
 
-                            # Open the pdf link in a new tab
-                            driver.execute_script("window.open();")
-                            driver.switch_to.window(driver.window_handles[-1])
-                            driver.get(pdf_url)
+                        # Close the PDF window/tab and switch back to the original window/tab
+                        driver.close()
+                        driver.switch_to.window(driver.window_handles[-1])
 
-                            # Wait until the PDF is loaded
-                            wait.until(lambda driver: driver.current_url != link_url)
-
-                            # Make download directory if it doesn't exist
-                            download_dir = DOWNLOAD_DIR / date_text
-                            if not download_dir.exists():
-                                download_dir.mkdir(parents=False)
-
-                            # Download the PDF file
-                            download_pdf(
-                                url=pdf_url,
-                                download_dir=download_dir,
-                                new_filename=section_title.replace("/", "_"),
-                            )
-
-                            # Close the PDF window/tab and switch back to the original window/tab
-                            driver.close()
-                            driver.switch_to.window(driver.window_handles[-1])
-
-            driver.close()  # Close this window
-            driver.switch_to.window(driver.window_handles[0])
+        driver.close()  # Close this window
+        driver.switch_to.window(driver.window_handles[0])
 
 
 if __name__ == "__main__":
